0240b3b66684cbd93ca89f3fe1c1f006e5137cb1
unknown <paul.kyalo@student.moringaschool.com>
2024-07-15 20:46:58 +0300

    created myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+//This will multiply the two numbers
+console.log(multiply);
+// This will output 62
 
-// Write your code here
+let random = Math.floor (Math.random() * 100) + 1;
+console.log(random);
+//This will output a number greater than 0
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+//This will output 4
+
+
+let numbers = [5,10,15,20];
+let max = Math.max (...numbers);
+console.log(Max);
+//This will output 20
